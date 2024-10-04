ca32adef0d0f25f7fc55737d74dfa08454beddca
Alex Chee <xxchee555xx@gmail.com>
2024-10-04 12:47:15 +0800

    🆕 NEW : add files for session 2 (start)

---
+++ client/src/components/TestComponent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+//TODO : Import onMounted
+
+const props = //TODO;
+//TODO : on mounted
+</script>
+
+<template>
+  <!-- TODO -->
+</template>
+
+<style scoped></style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref, computed, type Ref } from 'vue';
+//TODO : Import Test Component
+
+const username: //TODO;
+
+//v-if
+const userRole: //TODO;
+
+//v-for
+const fruits: //TODO;
+
+//v-model and ref reactivity
+const inputValue = ref('Starting Value');
+
+//TODO : Use a computed property to reactively update the HTML content when inputValue changes
+
+
+//TODO : counter
+</script>
+
+<template>
+  <main>
+    Hello world!
+    <!-- TODO -->
+  </main>
+</template>
