6c1c91e7ce40fdd08658ff2005c1ec7cd7b5387c
Alex Chee <xxchee555xx@gmail.com>
2024-10-04 12:50:05 +0800

    🆕 NEW : add code for session 2 (end)

--- client/src/components/TestComponent.vue
+++ client/src/components/TestComponent.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
-//TODO : Import onMounted
+import { onMounted } from 'vue';
 
-const props = //TODO;
-//TODO : on mounted
+const props = defineProps<{ username: string; age: number }>();
+const ages = props.age + 1;
+
+onMounted(() => {
+  console.log('Component has been mounted.');
+  console.log(ages);
+});
 </script>
 
 <template>
-  <!-- TODO -->
+  <h1>this is a test component</h1>
+  User : {{ props.username }}
+  <br />
+  Age: {{ props.age }}
+  <br />
+  <slot></slot>
+  <p><slot name="description"></slot></p>
 </template>
+<style scoped>
+h1 {
+  color: yellow;
+}
 
-<style scoped></style>
+p {
+  color: brown;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,27 +1,55 @@
 <script setup lang="ts">
 import { ref, computed, type Ref } from 'vue';
-//TODO : Import Test Component
+import TestComponent from '../components/TestComponent.vue';
 
-const username: //TODO;
+const username: string = 'Alex';
 
 //v-if
-const userRole: //TODO;
+//import type Ref, or let it auto infer
+const userRole: Ref<string> = ref('admin');
 
 //v-for
-const fruits: //TODO;
+const fruits: string[] = ['Apple', 'Banana', 'Orange', 'Grapes'];
 
 //v-model and ref reactivity
 const inputValue = ref('Starting Value');
+// Use a computed property to reactively update the HTML content when inputValue changes
+const htmlContent = computed(() => {
+  return `<p style="color: red;">${inputValue.value}</p>`;
+});
 
-//TODO : Use a computed property to reactively update the HTML content when inputValue changes
+const counter = ref(0);
 
-
-//TODO : counter
+const incrementCounter = () => {
+  counter.value++;
+};
 </script>
 
 <template>
   <main>
     Hello world!
-    <!-- TODO -->
+    {{ username }}
+    <TestComponent v-bind:age="22" username="username">
+      this is a slot!
+      <template v-slot:description>This is a description!</template>
+    </TestComponent>
   </main>
+
+  <p v-if="userRole === 'admin'">You are an Admin.</p>
+  <p v-else-if="userRole === 'user'">You are a regular User.</p>
+  <p v-else>You are a Guest. Please log in.</p>
+
+  <ul>
+    <li v-for="(fruit, index) in fruits" :key="index">{{ index + 1 }}. {{ fruit }}</li>
+  </ul>
+
+  <input
+    v-bind:value="inputValue"
+    v-on:input="(event) => (inputValue = (event.target as HTMLInputElement).value)"
+  />
+
+  <input v-model="inputValue" />
+  <div v-html="htmlContent"></div>
+
+  <h3 @click="incrementCounter">You've clicked the button {{ counter }} times.</h3>
 </template>
